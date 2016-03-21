e71de6fbe862db2281cf8bee5e391767acf05a7b
Ido Green <greenido@gmail.com>
2016-03-21 18:55:25 +0200

    readable date

--- public/js/main.js
+++ public/js/main.js
@@ -150,7 +150,7 @@
 	  var hour = a.getHours();
 	  var min = a.getMinutes();
 	  var sec = a.getSeconds();
-	  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+	  var time = date + '/' + month + '/' + year + ' - ' + hour + ':' + min + ':' + sec ;
 	  return time;
 	}
 
