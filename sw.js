c49945ca891fc4ac83f40793d8648688d94ca542
Ido Green <greenido@gmail.com>
2016-03-23 18:58:19 +0200

    More comments and cleaning the code

--- sw.js
+++ sw.js
@@ -4,9 +4,13 @@
  *  Author: Ido Green
  *  Date: March 2016
  */
- 
+
+// Until all the browsers will give us the APIs to cache.
 importScripts('/cache-polyfill.js');
 
+//
+// Install our web app and its assets
+//
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('noter').then(function(cache) {
@@ -27,13 +31,14 @@ self.addEventListener('install', function(e) {
   );
 });
 
+//
 self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim());
 });
 
+//
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
-  
+  console.log("SW got: " + event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
