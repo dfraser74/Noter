cfab1713675b92da9f6faecf0147311013109f35
Ido Green <greenido@gmail.com>
2016-03-23 17:07:48 +0200

    now with SW!

---
+++ sw.js
@@ -0,0 +1,42 @@
+/*
+ *
+ *  Noter
+ *  Author: Ido Green
+ *  Date: March 2016
+ */
+ 
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('noter').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+        'css/bootstrap-theme.min.css',
+        'css/bootstrap.min.css',
+        'js/vendor/modernizr-2.8.3-respond-1.4.2.min.js',
+        'js/vendor/jquery-1.11.2.min.js',
+        'js/vendor/bootstrap.min.js'
+      ]).then(function() {
+        return self.skipWaiting();
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
